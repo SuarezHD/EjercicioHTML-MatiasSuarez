89fa686dbeb3703276f63e36d742068b72ef3acd
Matias Suarez <mssuareez7@gmail.com>
2024-04-03 08:38:39 -0300

    Segundo commit

---
+++ script.js
@@ -0,0 +1,6 @@
+function sumar() {
+    const num1 = parseFloat(document.getElementById('numero1').value);
+    const num2 = parseFloat(document.getElementById('numero2').value);
+    const resultado = num1 + num2;
+    document.getElementById('resultado').textContent = `El resultado es: ${resultado}`;
+}
